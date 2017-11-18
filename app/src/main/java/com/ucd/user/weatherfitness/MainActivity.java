532c523bc9322b0f7f53695104788bde080223ec
harshitkumar0603 <harshit.kumar@ucdconnect.ie>
2017-11-18 12:44:56 +0000

    test branch

--- app/src/main/java/com/ucd/user/weatherfitness/MainActivity.java
+++ app/src/main/java/com/ucd/user/weatherfitness/MainActivity.java
@@ -2,16 +2,27 @@ package com.ucd.user.weatherfitness;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.ucd.user.weatherfitness.model.FetchWeatherTask;
+
+
 
 ////comment
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView score_id = (TextView)findViewById(R.id.score_ID);
+        FetchWeatherTask weatherTask = new FetchWeatherTask(score_id);
+        weatherTask.execute("7778677");
 // branch test
         // Added a comment - Kumar
     }
+
+
 }
